06e28c93bc6f2ae029b8bd3bc4b5eba48c2277f0
Tom ten Thij <git@tomtenthij.nl>
2009-11-01 12:49:55 +0000

    Require ruby-debug for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'grog'
 require 'spec'
 require 'spec/autorun'
+require 'ruby-debug'
 
 Spec::Runner.configure do |config|
   
